7175e5975de703cd81aa399fb0629bfe02ec6a68
Daniel Carral <dan@dcarral.org>
2015-08-30 09:21:14 +0200

    Remove siteconfig and set local db settings with dumb pass

--- environment.rb
+++ environment.rb
@@ -13,17 +13,12 @@ require 'sinatra' unless defined?(Sinatra)
 Dotenv.load
 
 configure do
-  SiteConfig = OpenStruct.new(
-                 :title => 'Your Application Name',
-                 :author => 'Your Name',
-                 :url_base => 'http://localhost:4567/'
-               )
-
   # load models
   $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
   Dir.glob("#{File.dirname(__FILE__)}/lib/*.rb") { |lib| require File.basename(lib, '.*') }
 
-  DataMapper.setup(:default, (ENV["DATABASE_URL"] || 'postgres://localhost/bukowski-on-sinatra'))
+  DataMapper.setup(:default, (ENV["DATABASE_URL"] || "postgres://dani:bukowski-on-sinatra-pass@localhost/bukowski-on-sinatra"))
+
   DataMapper.finalize
   DataMapper.auto_upgrade!
 end
